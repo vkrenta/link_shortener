b763dd3a749d162ebab4b0dbe3506b18ac79cef6
vkrenta <0vkrenta@gmail.com>
2020-03-11 18:41:32 +0200

    config project

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+
+app.listen(process.env.PORT, () =>
+  console.log('Listening port: ', process.env.PORT)
+);
