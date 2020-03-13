b7bf4d1a815665ced881e8b6624277679342deec
vkrenta <0vkrenta@gmail.com>
2020-03-13 22:43:52 +0200

    added error handling

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -1,12 +1,12 @@
 const { users } = require('../models');
-const errors = require('./errors');
+const { throwError } = require('./errors');
 
 const createUser = body =>
   users
     .create(body)
     .then(user => console.log('Created user ' + user))
     .catch(() => {
-      throw errors.get(5000);
+      throwError(5000);
     });
 
 module.exports = {
