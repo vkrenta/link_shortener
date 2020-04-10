1a7439a695829d1c9851302e7059c1a44193a819
vkrenta <0vkrenta@gmail.com>
2020-04-10 22:15:36 +0300

    reworked route to internal

--- client/src/routes/Internal.tsx
+++ client/src/routes/Internal.tsx
@@ -4,9 +4,12 @@ import { clearError } from '../actions';
 
 const Internal: React.FC = () => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(clearError());
-  });
+  useEffect(
+    () => () => {
+      dispatch(clearError());
+    },
+    [dispatch]
+  );
 
   return <h1>500: Internal server error</h1>;
 };
---
+++ client/src/sagas/errorHandler.tsx
@@ -0,0 +1,29 @@
+import { put, call } from 'redux-saga/effects';
+import {
+  internalError,
+  removeToken,
+  removeUser,
+  customError,
+} from '../actions';
+import React from 'react';
+import { Switch, Redirect } from 'react-router-dom';
+
+export default function* errorHandler(e: any) {
+  const { message, code } = JSON.parse(e.message);
+  if (code === 500) {
+    yield put(internalError(message));
+    yield call(ReturnInternal);
+  }
+  if (code === 6001) {
+    yield put(removeToken());
+    yield put(removeUser());
+  } else yield put(customError(code, message));
+}
+
+function ReturnInternal() {
+  return (
+    <Switch>
+      <Redirect to="/internal" />
+    </Switch>
+  );
+}
